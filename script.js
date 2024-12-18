2e453b98906d32938076b37c8be1ef72d394d02a
AdminC3A <supervision@casatresaguas.com>
2024-12-18 15:13:52 -0700

    Update script.js

--- script.js
+++ script.js
@@ -67,7 +67,7 @@ function onScanSuccess(decodedText) {
 
     if (validCodes.includes(decodedText)) {
         // Mostrar imagen de acceso permitido
-        validationImage.src = "images/Permitido.png";
+        validationImage.src = "images/Adios.png";
         validationImage.style.display = "block";
         document.getElementById("result").innerText = `Código detectado: ${decodedText} - Salida Registrada`;
 
